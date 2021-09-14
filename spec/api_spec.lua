672f9e0b0768528b11b16b2b00a63f9e38e28712
Ferronn <ferronn@ferronn.dev>
2021-09-14 15:54:38 +0000

    empty env applies to whole file, not just impl

--- spec/api_spec.lua
+++ spec/api_spec.lua
@@ -3,12 +3,12 @@ describe('api', function()
     if f:sub(-4) == '.lua' then
       local fn = f:sub(1, -5)
       describe(fn, function()
-        local t = dofile('api/' .. f)
+        local t = setfenv(loadfile('api/' .. f), {})()
         it('has the right name', function()
           assert.same(fn, t.name)
         end)
         if t.impl and t.tests then
-          local impl = setfenv(t.impl, {})
+          local impl = t.impl
           for _, test in ipairs(t.tests) do
             (test.pending and pending or it)(test.name, function()
               assert.same(test.outputs, {impl(unpack(test.inputs))})
